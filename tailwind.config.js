b93085cbc42c5a440882f91549f3b0ad513cc21e
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-03-25 20:01:32 +0100

    login page initial layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'login-image': "url('./public/assets/images/corgi_sit.jpg')",
+       },
+      colors: {
+        kLightGrey: '#C5C5C5',
+        kWhiteDark: '#F4F4F4',
+        kBlue:'#4971FF'
+      },
+    },
   },
   plugins: [],
 };
