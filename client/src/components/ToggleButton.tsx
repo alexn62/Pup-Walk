66bcafb9d2732bc82e8c4a55994fe37fe7f07484
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-04-21 13:17:55 +0200

    error validation for add job form

---
+++ client/src/components/ToggleButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ToggleButton = ({ label, active, onClick }: { label: string; active: boolean; onClick: (e: any) => void }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`border rounded-md border-kBlue  whitespace-nowrap px-4 h-9 transition-all ${
+        active ? 'bg-kBlue text-white' : 'bg-white text-kBlue'
+      }`}
+    >
+      {label}
+    </button>
+  );
+};
+export default ToggleButton;
--- client/src/views/AddJob.tsx
+++ client/src/views/AddJob.tsx
@@ -6,7 +6,12 @@ import TopBar from '../components/TopBar';
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import * as api from '../services/api.service';
 import debounce from 'lodash.debounce';
-import { isConstructorDeclaration } from 'typescript';
+import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import spinner from '../assets/icons/spinner.svg';
+import ToggleButton from '../components/ToggleButton';
+
+// Interfaces
 interface geoLoc {
   x: number; // lon
   y: number; // lat
@@ -15,8 +20,9 @@ interface geoLoc {
     [number, number], // south, west - lat, lon
     [number, number] // north, east - lat, lon
   ];
-  raw: any; // raw
+  raw?: any; // raw
 }
+
 type AddJobFormInputs = {
   title: string;
   details: string;
@@ -39,24 +45,50 @@ interface AddJobVariables {
   status: string;
 }
 
+interface Option {
+  label: string;
+  active: boolean;
+}
+
 const AddJob = () => {
+  // duration
+  const [durationOptions, setDurationOptions] = useState<Option[]>([
+    { label: '30', active: false },
+    { label: '60', active: false },
+    { label: '90', active: false },
+  ]);
+  const [duration, setDuration] = useState('');
+
+  // hourly pay
+  const [hourlyPayOptions, setHourlyPayOptions] = useState<Option[]>([
+    { label: '10', active: false },
+    { label: '15', active: false },
+    { label: '20', active: false },
+  ]);
+  const [hourlyPay, setHourlyPay] = useState('');
+
+  // location
   const [locationFocus, setLocationFocus] = useState(false);
   const [currentLocation, setCurrentLocation] = useState({
     x: 0,
     y: 0,
     label: '',
-    raw: '',
   });
+  const [loadingLocation, setLoadingLocation] = useState(false);
+
   const provider = useMemo(() => {
     return new OpenStreetMapProvider();
   }, []);
+
   const [searchResults, setSearchResults] = useState<geoLoc[]>([]);
+
   const getSearchResults = useMemo(() => {
     return debounce(async (query: string) => {
       const results = (await provider.search({ query: query })) as geoLoc[];
       setSearchResults((_) => results.slice(0, 10));
     }, 200);
   }, [provider]);
+
   const handleSearchQueryChange = (e: any) => {
     setCurrentLocation((prev) => ({ ...prev, label: e.target.value }));
     getSearchResults(e.target.value);
@@ -66,16 +98,46 @@ const AddJob = () => {
     setCurrentLocation((_) => searchResult);
   };
 
+  // form setup
   const {
+    getValues,
     register,
     handleSubmit,
+    setError,
+    clearErrors,
     formState: { errors },
   } = useForm<AddJobFormInputs>();
 
-  const [addNewJob, { /*error,*/ loading }] = useMutation(api.addJob);
+  const onSubmit: SubmitHandler<AddJobFormInputs> = async (data) => {
+    let valid = true;
+    clearErrors();
+    if (+duration < 15) {
+      setError('duration', { type: 'tooshort' });
+      valid = false;
+    }
+    if (+hourlyPay < 10) {
+      setError('hourlyPay', { type: 'toolittle' });
+      valid = false;
+    }
+    if (new Date() > new Date(getValues('startTime'))) {
+      setError('startTime', { type: 'inthepast' });
+      valid = false;
+    }
+    if (!currentLocation.x || !currentLocation.y) {
+      setError('locationString', { type: 'invalidlocation' });
+      valid = false;
+    }
+    if (!valid) return;
+    const variables = getVariables(data);
+
+    const response = await addNewJob({ variables: variables });
+    console.log(response);
+  };
+
+  // mutation
+  const [addNewJob] = useMutation(api.addJob);
 
   const getVariables = (input: AddJobFormInputs): AddJobVariables => {
-    console.log(input);
     return {
       user: '624efadb989d6d26dfd1e55d',
       dog: '624efc55989d6d26dfd1e55f',
@@ -83,30 +145,11 @@ const AddJob = () => {
       details: input.details,
       latitude: currentLocation.y,
       longitude: currentLocation.x,
-      duration: +input.duration,
-      hourlyPay: +input.hourlyPay,
+      duration: +duration,
+      hourlyPay: +hourlyPay,
       startTime: input.startTime.toString(),
       status: 'OPEN',
     };
-  };
-
-  const onSubmit: SubmitHandler<AddJobFormInputs> = async (data) => {
-    const variables = getVariables(data);
-    console.log(variables);
-    const mockVariables = {
-      user: '624efadb989d6d26dfd1e55d',
-      dog: '624efc55989d6d26dfd1e55f',
-      title: 'EXAMPLE2',
-      details: 'Frankie needs her walk today or she will be super mad later today...',
-      latitude: 53,
-      longitude: 13,
-      duration: 75,
-      hourlyPay: 16,
-      startTime: '2022-04-08T15:23:22.176+00:00',
-      status: 'OPEN',
-    };
-    const response = await addNewJob({ variables: variables });
-    console.log(response);
   };
 
   return (
@@ -114,36 +157,107 @@ const AddJob = () => {
       <TopBar title="Add Job"></TopBar>
       <div className="p-3 pt-16">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            placeholder="Title..."
-            {...register('title', { required: true })}
-            className={`w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
-              errors.title ? 'border-red-400' : 'border'
-            }`}
-          ></input>
-          <textarea
-            placeholder="Details..."
-            {...register('details', { required: true })}
-            className={`h-24 w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
-              errors.details ? 'border-red-400' : 'border'
-            }`}
-          ></textarea>
-          <input
-            type="number"
-            placeholder="Duration..."
-            {...register('duration', { required: true })}
-            className={`w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
-              errors.duration ? 'border-red-400' : 'border'
-            }`}
-          ></input>
-          <input
-            type="number"
-            placeholder="Hourly pay..."
-            {...register('hourlyPay', { required: true })}
-            className={`w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
-              errors.hourlyPay ? 'border-red-400' : 'border'
-            }`}
-          ></input>
+          <label>
+            Title:
+            <input
+              placeholder="Quick walk around the block..."
+              {...register('title', { required: true })}
+              className={`w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
+                errors.title ? 'border-red-400' : 'border'
+              }`}
+            ></input>
+          </label>
+          {errors.title?.type === 'required' && (
+            <p className="text-sm text-red-500 text-right">This field is required</p>
+          )}
+          <label>
+            Details:
+            <textarea
+              placeholder="The pup needs to get outside..."
+              {...register('details', { required: true, maxLength: 150 })}
+              className={`h-24 w-full rounded-md border focus:border-kBlue p-2 mt-1 text-sm focus:outline-none ${
+                errors.details ? 'border-red-400' : 'border'
+              }`}
+            ></textarea>
+          </label>
+          {errors.details?.type === 'required' && (
+            <p className="text-sm text-red-500 text-right">This field is required</p>
+          )}
+          {errors.details?.type === 'maxLength' && <p className="text-sm text-red-500 text-right">Too long</p>}
+          <label>
+            Duration:
+            <div className="flex flex-row items-center space-x-2">
+              <input
+                disabled={durationOptions.some((el) => el.active)}
+                type="number"
+                placeholder="45"
+                value={duration}
+                {...register('duration', {
+                  onChange: (e: any) => {
+                    setDuration(e.target.value);
+                  },
+                })}
+                className={`w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
+                  errors.duration ? 'border-red-400' : 'border'
+                }`}
+              ></input>
+              {durationOptions.map((option) => (
+                <ToggleButton
+                  key={option.label}
+                  label={option.label}
+                  active={option.active}
+                  onClick={(e: any) => {
+                    e.preventDefault();
+                    clearErrors('duration');
+                    setDuration(option.label);
+                    setDurationOptions((prev) =>
+                      [...prev].map((el) => ({ ...el, active: option.label === el.label ? !el.active : false }))
+                    );
+                  }}
+                ></ToggleButton>
+              ))}
+              <p>min</p>
+            </div>
+          </label>
+          {errors.duration && <p className="text-sm text-red-500 text-right">The minimum duration is 15 minutes</p>}
+
+          <label>
+            Hourly Pay:
+            <div className="flex flex-row items-center space-x-2">
+              <input
+                disabled={hourlyPayOptions.some((el) => el.active)}
+                type="number"
+                placeholder="18.00"
+                value={hourlyPay}
+                {...register('hourlyPay', {
+                  onChange: (e: any) => {
+                    setHourlyPay(e.target.value);
+                  },
+                })}
+                className={`w-full rounded-md border focus:border-kBlue p-2 my-1 text-sm focus:outline-none ${
+                  errors.hourlyPay ? 'border-red-400' : 'border'
+                }`}
+              ></input>
+              {hourlyPayOptions.map((option) => (
+                <ToggleButton
+                  key={option.label}
+                  label={option.label}
+                  active={option.active}
+                  onClick={(e: any) => {
+                    e.preventDefault();
+                    clearErrors('hourlyPay');
+                    setHourlyPay(option.label);
+                    setHourlyPayOptions((prev) =>
+                      [...prev].map((el) => ({ ...el, active: option.label === el.label ? !el.active : false }))
+                    );
+                  }}
+                ></ToggleButton>
+              ))}
+              <p>USD</p>
+            </div>
+          </label>
+          {errors.hourlyPay && <p className="text-sm text-red-500 text-right">The minimum pay is $10.00</p>}
+          <label>Date and time:</label>
           <input
             type="datetime-local"
             {...register('startTime', { required: true })}
@@ -151,14 +265,18 @@ const AddJob = () => {
               errors.startTime ? 'border-red-400' : 'border'
             }`}
           ></input>
-          <div className="flex flex-row relative">
-            <div>
+          {errors.startTime && <p className="text-sm text-red-500 text-right">Invalid Date</p>}
+
+          <label>Location:</label>
+          <div className="flex flex-row relative items-center">
+            <div className="flex-grow mr-2">
               <input
+                disabled={loadingLocation}
+                autoComplete="off"
                 onFocus={() => setLocationFocus(true)}
-                placeholder="Location..."
-                value={currentLocation.label}
+                placeholder="Berlin..."
+                value={loadingLocation ? 'Loading...' : currentLocation.label}
                 {...register('locationString', {
-                  required: true,
                   onChange: (e) => handleSearchQueryChange(e),
                   onBlur: () => setLocationFocus(false),
                 })}
@@ -166,6 +284,8 @@ const AddJob = () => {
                   errors.locationString ? 'border-red-400' : 'border'
                 }`}
               ></input>
+              {errors.locationString && <p className="text-sm text-red-500 text-right">Invalid location</p>}
+
               {locationFocus && searchResults.length > 0 && (
                 <div className="absolute top-12 w-full bg-white border border-kBlue p-2 flex flex-col max-h-48 overflow-scroll">
                   {searchResults.map((el) => (
@@ -187,7 +307,36 @@ const AddJob = () => {
                 </div>
               )}
             </div>
-            <button className="bg-kBlue p-2 rounded-full text-white">📍</button>
+            {loadingLocation ? (
+              <img src={spinner} alt="spinner"></img>
+            ) : (
+              <FontAwesomeIcon
+                onClick={() => {
+                  setLoadingLocation(true);
+                  clearErrors('locationString');
+                  navigator.geolocation.getCurrentPosition(async (position) => {
+                    if (position) {
+                      setLoadingLocation(false);
+                      const response = await fetch(
+                        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=en`
+                      );
+                      const data = await response.json();
+                      setCurrentLocation({
+                        label: data.locality,
+                        x: position.coords.longitude,
+                        y: position.coords.latitude,
+                      });
+                    } else {
+                      setLoadingLocation(false);
+                    }
+                  });
+                }}
+                icon={faLocationCrosshairs}
+                size="lg"
+                color="rgb(73 113 255)"
+                className=" p-2 "
+              />
+            )}
           </div>
           <div className="flex flex-row space-x-2 fixed bottom-0 w-full right-0 p-3">
             <MainButton title="Cancel" invert={true}></MainButton>
