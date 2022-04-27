ba85c0e53d7b4266cb4c561613948bdc7634fa98
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-04-27 09:45:44 +0200

    working on readme and added example env

--- server/index.ts
+++ server/index.ts
@@ -2,24 +2,19 @@ import { ApolloServer } from 'apollo-server';
 import resolvers from './resolvers/resolvers';
 import typeDefs from './typedefs/typedefs';
 import mongoose from 'mongoose';
-
+import 'dotenv/config';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
-    const token = req.headers.authorization || '';
-    const user = '';
-    return { user };
-  },
 });
 
-const SERVER_PORT = 4000;
-const HOST = 'localhost:';
-const DB_PORT = 27017;
-const DB_NAME = 'dogdb';
-const SERVER_URL = `http://${HOST}${SERVER_PORT}`;
+const SERVER_PORT = Number(process.env.SERVER_PORT);
+const HOST = process.env.HOST;
+const DB_PORT = Number(process.env.DB_PORT);
+const DB_NAME = process.env.DB_NAME;
+const SERVER_URL = `http://${HOST}:${SERVER_PORT}`;
 
-mongoose.connect(`mongodb://${HOST}${DB_PORT}/${DB_NAME}`);
+mongoose.connect(`mongodb://${HOST}:${DB_PORT}/${DB_NAME}`);
 
 const startServer = async (): Promise<void> => {
   server.listen(SERVER_PORT, () => {
